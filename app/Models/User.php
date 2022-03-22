3e6fc00d8996811a42b4bd3cb38adae816583796
mdiktushar <md.tushar1804@gmail.com>
2022-03-22 15:43:40 +0600

    relation test

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role(Type $var = null)
+    {
+        # code...
+        return $this->belongsTo(Role::class);
+    }
 }
