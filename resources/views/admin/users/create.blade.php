3efb48126071e10df715d0e32bea0bfb242e1728
mdiktushar <md.tushar1804@gmail.com>
2022-03-23 23:51:26 +0600

    laravel collective html package

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
     <h1>Create Users</h1>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" class="form-control" name="" id="name" name="name">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    
 @endsection
