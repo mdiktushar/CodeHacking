52db84b23441329269fcd864ba4ac34e350646c9
mdiktushar <md.tushar1804@gmail.com>
2022-04-30 14:56:02 +0600

    navbar folders

---
+++ resources/views/includes/admin_nav.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">Home</a>
+    </div>
+    <!-- /.navbar-header -->
+
+
+{{-- Top Nav --}}
+    @include('includes.admin_top_nav')
+
+
+
+{{-- Sidebar --}}
+    @include('includes.admin_side_nav')
+
+    <!-- /.navbar-static-side -->
+</nav>
